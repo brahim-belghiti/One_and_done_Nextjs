993e8fea1585105974c786fd7cff45795dcc68eb
brahim.belghiti <brahim.belghiti@proton.me>
2023-06-28 16:34:49 +0100

    refactor: use a regex to extract the videoId from the user link

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b1299f0adae42adf"; }
+/******/ 		__webpack_require__.h = function() { return "56de1c8855007e68"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
