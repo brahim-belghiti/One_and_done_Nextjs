6f33b0125dc66d4902a9c64ca8dcb2a84b3160e4
brahim-belghiti <brahim.belghiti@proton.me>
2024-10-27 20:21:53 +0100

    update

--- src/components/takeNotesEditor.tsx
+++ src/components/takeNotesEditor.tsx
@@ -4,48 +4,23 @@ import {
   MDXEditor,
   headingsPlugin,
   listsPlugin,
-  quotePlugin,
-  thematicBreakPlugin,
-  markdownShortcutPlugin,
-  AdmonitionDirectiveDescriptor,
   codeBlockPlugin,
-  codeMirrorPlugin,
-  diffSourcePlugin,
-  directivesPlugin,
-  frontmatterPlugin,
-  imagePlugin,
-  linkDialogPlugin,
-  linkPlugin,
-  sandpackPlugin,
-  tablePlugin,
-  KitchenSinkToolbar,
-  toolbarPlugin,
-  InsertFrontmatter,
-  Headings,
-  UndoRedo,
-  BoldItalicUnderlineToggles,
-  ListsToggle,
 } from '@mdxeditor/editor';
 import '@mdxeditor/editor/style.css';
 
 function MdxEditor() {
   return (
     <section className="flex w-full h-full border-2 border-orange-400">
-        <MDXEditor
-      markdown="Hello world"
-      plugins={[
-        toolbarPlugin({
-          toolbarContents: () => (
-            <>
-              {' '}
-              <ListsToggle/>
-              <BoldItalicUnderlineToggles />
-            </>
-          )
-        })
-      ]}
-    />
-
+      <MDXEditor
+        markdown="Hello world"
+        plugins={[
+          headingsPlugin({
+            allowedHeadingLevels: [1, 2, 3, 4, 5, 6],
+          }),
+          listsPlugin(),
+          codeBlockPlugin(),
+        ]}
+      />
     </section>
   );
 }
